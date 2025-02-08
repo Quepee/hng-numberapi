9c8503f9a61fe1edaee866669378babe1a401e04
pratt <xcellersqp@gmail.com>
2025-02-08 08:59:30 +0100

    new file

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const classifyRoutes = require('./src/routes/classifyRoutes');
+const classifyRoutes = require('../src/routes/classifyRoutes');
 
 const app = express();
 
